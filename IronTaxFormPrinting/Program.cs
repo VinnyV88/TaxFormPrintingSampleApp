2174bcb021193230aada534addc63c7092b97524
Vincent Laptop <vincent@vividwebsolutions.net>
2023-02-01 19:04:02 -0500

    Add project files.

---
+++ IronTaxFormPrinting/Program.cs
@@ -0,0 +1,60 @@
+﻿using IronPdf;
+// Don't necessarily need a license key, but wanted to show apples to apples. 
+// I removed my trial license key
+//License.LicenseKey = "";
+
+string path = Directory.GetCurrentDirectory();
+
+string fileName = $"{path}\\PDFs\\IronPDF_W2_2022_01[1_to_250].pdf";
+
+var pdfs = new List<PdfDocument>();
+
+
+using (FileStream outputFileStream = new FileStream(fileName, FileMode.Create, FileAccess.ReadWrite))
+{
+    for (int i = 1; i <= 250; i++)
+    {
+        PdfDocument doc = PdfDocument.FromFile($"{path}\\Template\\w2_template.pdf");
+
+        var loadedForm = doc.Form;
+
+        loadedForm.GetFieldByName("WSSN").Value = "123456789";
+        loadedForm.GetFieldByName("WYEAR").Value = "2022";
+        loadedForm.GetFieldByName("WCNTRLNUM").Value = i.ToString("D8");
+        loadedForm.GetFieldByName("WEMPIDNUM").Value = "111222333444";
+        loadedForm.GetFieldByName("WEMPNAME1").Value = "EMPLOYER NAME 1";
+        loadedForm.GetFieldByName("WEMPNAME2").Value = "EMPLOYER NAME 2";
+        loadedForm.GetFieldByName("WEADDLN1").Value = "ADDRESS LINE 1";
+        loadedForm.GetFieldByName("WEADDLN2").Value = "ADDRESS LINE 2";
+        loadedForm.GetFieldByName("WMUSNAME").Value = "EMPLOYEE NAME";
+        loadedForm.GetFieldByName("WMADDLN1").Value = "ADDRESS LINE 1";
+        loadedForm.GetFieldByName("WMADDLN2").Value = "ADDRESS LINE 2";
+        loadedForm.GetFieldByName("WMADDLN3").Value = "ADDRESS LINE 3";
+        loadedForm.GetFieldByName("WMADDLN4").Value = "ADDRESS LINE 4";
+        loadedForm.GetFieldByName("WMADDLN5").Value = "ADDRESS LINE 5";
+        loadedForm.GetFieldByName("WFEDWAGTPS").Value = "50,000.00";
+        loadedForm.GetFieldByName("WFEDINCTAX").Value = "5,000.00";
+        loadedForm.GetFieldByName("WSSWAGES").Value = "50,000.00";
+        loadedForm.GetFieldByName("WSSTAX").Value = "500.00";
+        loadedForm.GetFieldByName("WMEDWAGTPS").Value = "50,000.00";
+        loadedForm.GetFieldByName("WMEDWAGTAX").Value = "500.00"; ;
+        loadedForm.GetFieldByName("WSTATE").Value = "NY";
+        loadedForm.GetFieldByName("WSTAIDNUM").Value = "987654321";
+        loadedForm.GetFieldByName("WSTAWAGTPS").Value = "50,000.00"; 
+        loadedForm.GetFieldByName("WSTAWAGTAX").Value = "200.00";
+        loadedForm.GetFieldByName("WLOCNAMES").Value = "NEW YORK";
+        loadedForm.GetFieldByName("WLOCWAGTPS").Value = "0.00";
+        loadedForm.GetFieldByName("WLOCWAGTAX").Value = "0.00";
+        loadedForm.GetFieldByName("WOTHER").Value = "25.75      NYUI";
+
+        doc.Flatten();
+        pdfs.Add(doc);
+        Console.WriteLine($"Append document {i}");
+
+    }
+    var pdf = PdfDocument.Merge(pdfs);
+
+    Console.WriteLine("Save File!");
+    outputFileStream.Write(pdf.BinaryData);
+}
+Console.WriteLine("FINISHED!!!");
---
+++ TaxFormPrintingSample/Program.cs
@@ -0,0 +1,73 @@
+﻿using Syncfusion.Licensing;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Parsing;
+
+// Use your Syncfusion License Key Here
+//SyncfusionLicenseProvider.RegisterLicense("");
+
+string path = Directory.GetCurrentDirectory(); 
+
+PdfDocument document = new PdfDocument();
+
+//Set EnableMemoryOptimization to true
+document.EnableMemoryOptimization = true;
+
+string fileName = $"{path}\\PDFs\\SyncFusion_W2_2022_01[1_to_250].pdf";
+//Create output file stream.
+using (FileStream outputFileStream = new FileStream(fileName, FileMode.Create, FileAccess.ReadWrite))
+{
+    for (int i = 1; i <= 250; i++)
+    {
+        Stream fileIn = new FileStream($"{path}\\Template\\w2_template.pdf", FileMode.Open, FileAccess.Read, FileShare.Read);
+        // Create a byte array of file stream length  
+        byte[] pdfData = new byte[fileIn.Length];
+        //Read block of bytes from stream into the byte array   
+        fileIn.Read(pdfData, 0, Convert.ToInt32(pdfData.Length));
+        // Load the byte array 
+        PdfLoadedDocument loadedDocument = new PdfLoadedDocument(pdfData);
+        loadedDocument.Compression = PdfCompressionLevel.Best;
+        //Get the loaded form
+        PdfLoadedForm loadedForm = loadedDocument.Form;
+
+        (loadedForm.Fields["WSSN"] as PdfLoadedTextBoxField).Text = "123456789";
+        (loadedForm.Fields["WYEAR"] as PdfLoadedTextBoxField).Text = "2022";
+        (loadedForm.Fields["WCNTRLNUM"] as PdfLoadedTextBoxField).Text = i.ToString("D8");
+        (loadedForm.Fields["WEMPIDNUM"] as PdfLoadedTextBoxField).Text = "111222333444";
+        (loadedForm.Fields["WEMPNAME1"] as PdfLoadedTextBoxField).Text = "EMPLOYER NAME 1";
+        (loadedForm.Fields["WEMPNAME2"] as PdfLoadedTextBoxField).Text = "EMPLOYER NAME 2";
+        (loadedForm.Fields["WEADDLN1"] as PdfLoadedTextBoxField).Text = "ADDRESS LINE 1";
+        (loadedForm.Fields["WEADDLN2"] as PdfLoadedTextBoxField).Text = "ADDRESS LINE 2";
+        (loadedForm.Fields["WMUSNAME"] as PdfLoadedTextBoxField).Text = "EMPLOYEE NAME";
+        (loadedForm.Fields["WMADDLN1"] as PdfLoadedTextBoxField).Text = "ADDRESS LINE 1";
+        (loadedForm.Fields["WMADDLN2"] as PdfLoadedTextBoxField).Text = "ADDRESS LINE 2";
+        (loadedForm.Fields["WMADDLN3"] as PdfLoadedTextBoxField).Text = "ADDRESS LINE 3";
+        (loadedForm.Fields["WMADDLN4"] as PdfLoadedTextBoxField).Text = "ADDRESS LINE 4";
+        (loadedForm.Fields["WMADDLN5"] as PdfLoadedTextBoxField).Text = "ADDRESS LINE 5";
+        (loadedForm.Fields["WFEDWAGTPS"] as PdfLoadedTextBoxField).Text = "50,000.00";
+        (loadedForm.Fields["WFEDINCTAX"] as PdfLoadedTextBoxField).Text = "5,000.00";
+        (loadedForm.Fields["WSSWAGES"] as PdfLoadedTextBoxField).Text = "50,000.00";
+        (loadedForm.Fields["WSSTAX"] as PdfLoadedTextBoxField).Text = "500.00";
+        (loadedForm.Fields["WMEDWAGTPS"] as PdfLoadedTextBoxField).Text = "50,000.00";
+        (loadedForm.Fields["WMEDWAGTAX"] as PdfLoadedTextBoxField).Text = "500.00"; ;
+        (loadedForm.Fields["WSTATE"] as PdfLoadedTextBoxField).Text = "NY";
+        (loadedForm.Fields["WSTAIDNUM"] as PdfLoadedTextBoxField).Text = "987654321";
+        (loadedForm.Fields["WSTAWAGTPS"] as PdfLoadedTextBoxField).Text = "50,000.00"; 
+        (loadedForm.Fields["WSTAWAGTAX"] as PdfLoadedTextBoxField).Text = "200.00";
+        (loadedForm.Fields["WLOCNAMES"] as PdfLoadedTextBoxField).Text = "NEW YORK";
+        (loadedForm.Fields["WLOCWAGTPS"] as PdfLoadedTextBoxField).Text = "0.00";
+        (loadedForm.Fields["WLOCWAGTAX"] as PdfLoadedTextBoxField).Text = "0.00";
+        (loadedForm.Fields["WOTHER"] as PdfLoadedTextBoxField).Text = "25.75      NYUI";
+
+        //Flatten the whole form.
+        loadedForm.Flatten = true;
+        document.Append(loadedDocument);
+        Console.WriteLine($"Append document {i}");
+        loadedDocument.Close(true);
+
+    }
+    Console.WriteLine("Save File!");
+    document.Save(outputFileStream);
+    document.Close(true);
+}
+Console.WriteLine("FINISHED!!!");
+
